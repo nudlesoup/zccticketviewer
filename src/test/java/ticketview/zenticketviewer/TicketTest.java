f870b2d4026fb172e2bb4d4ba4e2801a0c2ca038
nudlesoup <ameyad1995@gmail.com>
2021-11-28 03:14:10 -0700

    Added Unit Tests for Ticket Class methods

---
+++ src/test/java/ticketview/zenticketviewer/TicketTest.java
@@ -0,0 +1,52 @@
+package ticketview.zenticketviewer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ticketview.zenticketviewer.model.Ticket;
+
+/*Unit tests for Ticket class methods */
+public class TicketTest{
+
+	//Tested Method: showTicket()
+	@Test
+	//Id = 0 -> SUCCESS
+	public void showTicketWithId0() throws Exception {
+		Ticket ticket = new Ticket();
+		//Execution
+		String result = ticket.showTicket(0);
+		//Verification
+		assertEquals("ERROR: ID must be > 0",result);
+	}
+	
+	@Test
+	//Valid id - SUCCESS
+	public void showTicketWithIdValid() throws Exception {
+		Ticket ticket = new Ticket();
+		//Execution
+		String result = ticket.showTicket(3);
+		//Verification
+		assertEquals("SUCCESS",result);
+	}
+	
+	@Test
+	//Id < 0 -> ERROR
+	public void showTicketWithIdMinus() throws Exception {
+		Ticket ticket = new Ticket();
+		//Execution
+		String result = ticket.showTicket(-1);
+		//Verification
+		assertEquals("ERROR: ID must be > 0",result);
+	}
+	
+	@Test
+	//Id not exist -> ERROR
+	public void showTicketWithIdNotExist() throws Exception {
+		Ticket ticket = new Ticket();
+		//Execution
+		String result = ticket.showTicket(123456);
+		//Verification
+		assertEquals("Not Found - unable to locate the requested file or resource",result);
+	}
+}
